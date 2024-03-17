da2421eb89bd326fc74816481f4b672c0588be48
Raphael Cunha <raphaelsrcunha@gmail.com>
2024-03-17 09:03:24 -0400

    API almost done

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        /* Sync method
+                public ActionResult<List<Product>> GetProducts()
+        {
+            var products = context.Products.ToList();
+
+            return Ok(products);
+        }
+        */
+
+        [HttpGet("{id}")] //api/products/3
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        /* Sync method
+        public ActionResult<Product> GetProduct(int id)
+        {
+            return context.Products.Find(id);
+        }
+        */
+    }
+}
